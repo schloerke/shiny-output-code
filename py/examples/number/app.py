7235c34e02eacf9b097cbd7b1f3193473c3c7163
Barret Schloerke <barret@posit.co>
2023-08-31 10:00:06 -0400

    Add example app

--- py/examples/number/app.py
+++ py/examples/number/app.py
@@ -1,21 +1,51 @@
 # pyright: reportUnusedFunction=false
 
-from shiny import App, Inputs, Outputs, Session, render, ui
-
-from shinyoutputcode import example_number_input
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+from shinyoutputcode import output_code, render_code
 
 app_ui = ui.page_fluid(
-    ui.h5("Number Input"),
-    example_number_input("x", value=10, min=5, max=15),
-    ui.output_text_verbatim("txt"),
+    ui.h5("Text Input"),
+    "Language: ",
+    ui.input_switch("sw", "R", value=False),
+    # ui.input_action_button("btn", "Set Code"),
+    ui.input_text_area("txt", "my label", value="1 + 1"),
+    ui.output_text_verbatim("txt_out"),
+    output_code("code"),
 )
 
 
 def server(input: Inputs, output: Outputs, session: Session):
     @output
     @render.text
-    def txt():
-        return f"x: {input.x()}"
+    def txt_out():
+        return f"{input.txt()}"
+
+    @reactive.Effect
+    def _():
+        ui.update_switch("sw", label=language())
+
+    @reactive.Calc
+    def language() -> str:
+        if input.sw():
+            return "python"
+        else:
+            return "R"
+
+    @reactive.Calc
+    def copy_icon():
+        if input.sw():
+            return "🎉"
+        else:
+            return "🎊"
+
+    @reactive.Calc
+    def title():
+        f"{language()} Title"
+
+    @output
+    @render_code(title=title, language=language)
+    def code():
+        return f"[1, 2, 3]\n{input.txt()}"
 
 
 app = App(app_ui, server, debug=True)
--- py/shinyoutputcode/__init__.py
+++ py/shinyoutputcode/__init__.py
@@ -2,59 +2,10 @@ from __future__ import annotations
 
 __version__ = "0.0.1"
 
-__all__ = ("example_number_input",)
+from ._render import render_code
+from ._ui import output_code
 
-
-from pathlib import PurePath
-
-from htmltools import HTMLDependency, Tag, TagAttrs, TagAttrValue, TagChild, TagList
-
-
-def example_number_input(
-    id: str, *args: TagChild | TagAttrs, _add_ws: bool = True, **kwargs: TagAttrValue
-) -> TagList:
-    """
-    Create a <example-number-input> tag.
-
-    A WebComponent for creating number inputs.
-
-    Parameters
-    ----------
-    *args
-        Child elements to this tag.
-    _add_ws
-        Whether whitespace should be added around this tag.
-    **kwargs
-        Attributes to this tag.
-
-    Returns
-    -------
-    :
-        Tag
-
-    See Also
-    --------
-    ~htmltools.Tag
-    """
-
-    return TagList(
-        component_dep(),
-        Tag("example-number-input", id=id, *args, _add_ws=_add_ws, **kwargs),
-    )
-
-
-def component_dep() -> HTMLDependency:
-    ex_www_path = PurePath(__file__).parent / "www"
-
-    return HTMLDependency(
-        name="shinyoutputcode",
-        version=__version__,
-        source={
-            "package": "shinyoutputcode",
-            "subdir": str(ex_www_path),
-        },
-        stylesheet={"href": "open-props.min.css"},
-        script=[
-            {"src": "example-number-input.js", "type": "module"},
-        ],
-    )
+__all__ = (
+    "output_code",
+    "render_code",
+)
